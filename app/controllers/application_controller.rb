122e6486bbb4b66a066ae0ba09ed99ff7be95d75
miyukiimai <m3030710121@yahoo.co.jp>
2021-11-19 01:21:56 +0900

    バリデーションの訂正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :birthday, :first_name, :last_name, :first_name_kana, :last_name_kana])
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-         validates :name, presence: true
+         
          validates :nickname, presence: true
          validates :email, presence: true
          validates :encrypted_password, presence: true
