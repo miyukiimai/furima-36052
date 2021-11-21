cffc380f1b13fea3d2280e5f8e1b81dc794fd51c
miyukiimai <m3030710121@yahoo.co.jp>
2021-11-21 17:51:37 +0900

    単体テストコード入力

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,23 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
          validates :nickname, presence: true
-         validates :email, presence: true
-         validates :encrypted_password, presence: true
+         
+        
          validates :last_name, presence: true
          validates :first_name, presence: true
          validates :last_name_kana, presence: true
          validates :first_name_kana, presence: true
          validates :birthday, presence: true
+
+         with_options presence: true do
+         validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "Full-width characters"}
+         validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "Full-width characters"}
+         validates :last_name_kana, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "Full-width characters"}
+         validates :first_name_kana, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "Full-width characters"}
+
+         end
+
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+
 end
