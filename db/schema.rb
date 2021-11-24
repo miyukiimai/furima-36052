4f7e078a498527b98a3c308562cb0c6aeb6e7dc2
miyukiimai <m3030710121@yahoo.co.jp>
2021-11-24 18:11:02 +0900

    itemテーブルの再作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_23_064727) do
+ActiveRecord::Schema.define(version: 2021_11_24_090518) do
+
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -65,6 +66,8 @@ ActiveRecord::Schema.define(version: 2021_11_23_064727) do
     t.integer "delivery_date_id", null: false
     t.string "price", null: false
     t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
@@ -86,9 +89,7 @@ ActiveRecord::Schema.define(version: 2021_11_23_064727) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "addresses", "buys"
   add_foreign_key "items", "users"
-
 end
