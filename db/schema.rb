cc0d31cd55ab09f397ac3e156ae19959b1ad4f04
miyukiimai <m3030710121@yahoo.co.jp>
2021-11-24 18:03:08 +0900

    コンフリクト解消２

--- db/schema.rb
+++ db/schema.rb
@@ -86,4 +86,9 @@ ActiveRecord::Schema.define(version: 2021_11_23_064727) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "addresses", "buys"
+  add_foreign_key "items", "users"
+
 end
