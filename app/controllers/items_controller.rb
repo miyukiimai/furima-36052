dfe03cf1fff12abef41b50b3341b4a02b49dde55
miyukiimai <m3030710121@yahoo.co.jp>
2021-11-24 16:30:21 +0900

    コンフリクト解消

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new,:edit]
+
   def index
+    @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path(@item)
+    else
+      render :index
+    end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :item_description, :category_id, :product_condition_id, :shipping_cost_id, :prefecture_id, :delivery_date_id, :price, :image).merge(user_id: current_user.id)
+  end
+end
 end
