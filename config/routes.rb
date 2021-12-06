e90ccdbf2bd56de706bb3732a184936517a3c756
miyukiimai <m3030710121@yahoo.co.jp>
2021-12-06 12:43:12 +0900

    編集削除ボタン表示

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
  root to: "items#index"
  resources :items do
-   #resources :users, only: [:new, :create ]
+   resources :users, only: [:show]
  end 
 end
 
