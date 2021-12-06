59f4bbc5fba18f1af2677b4a19e31065ab1dbea3
miyukiimai <m3030710121@yahoo.co.jp>
2021-12-06 15:38:39 +0900

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- root to: "items#index"
- resources :items do
-   resources :users, only: [:show]
- end 
+  root to: "items#index"
+  resources :items  
 end
 
