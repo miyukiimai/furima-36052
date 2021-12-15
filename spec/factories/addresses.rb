a14017d78859a4e0e502ad6bc9d1939f982c856b
miyukiimai <m3030710121@yahoo.co.jp>
2021-12-15 12:17:21 +0900

    単体テストコード記述

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :address do
+    postal_code        { '123-1233' }
+    prefecture_id      { '2' }
+    city               { '市町村' }
+    house_number       { '129' }
+    building_number    { '建物929' }
+    phone_number       { '12345678901' }
     
   end
 end
